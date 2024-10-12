b884b574eb1adeaae5853912a9dc226e363af355
titch <sujinlee0202@gmail.com>
2024-10-12 16:20:48 +0900

    feat: 블로그 메타데이터 및 레이아웃 수정
    - 블로그 이름, 프로필 이미지 수정
    - 홈 메뉴 수정
    - Post 하단 이메일 뉴스레터 구독 삭제
    - Post 트위터 연동 기능 삭제
    - 댓글 기능 삭제

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,8 +1,8 @@
 const headerNavLinks = [
   { href: '/', title: 'Home' },
   { href: '/blog', title: 'Posts' },
-  { href: '/tags', title: 'Tags' },
-  { href: '/projects', title: 'Projects' },
+  // { href: '/tags', title: 'Tags' },
+  // { href: '/projects', title: 'Projects' },
   { href: '/about', title: 'About' },
 ]
 
